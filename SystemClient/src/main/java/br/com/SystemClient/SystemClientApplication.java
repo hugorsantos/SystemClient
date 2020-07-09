b5df6f1ffe291ebfd2109335ea6544a4bf76de4b
Hugo <hugorsantos@gmail.com>
2020-07-09 20:54:26 -0300

    First Commit

---
+++ SystemClient/src/main/java/br/com/SystemClient/SystemClientApplication.java
@@ -0,0 +1,13 @@
+package br.com.SystemClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemClientApplication.class, args);
+	}
+
+}
---
+++ SystemClient/src/test/java/br/com/SystemClient/SystemClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.SystemClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
